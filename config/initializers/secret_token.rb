b80cfa05b07b35375f8770d6064e827ca1a722fd
Felix Flores <felix@edgecase.com>
2011-06-27 20:34:29 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HappyBirthday::Application.config.secret_token = '831ecb5b97bbd9dcc32f5090473832e4bb1897ca1aadc4b0f6de4258d6c8829ea5cfe2ba9e21e19d8c89cfe827f1508bcc4fad3f3734ac3cfed890c5c3315a9f'
