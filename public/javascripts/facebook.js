8856f60834ef50244b176159d989bdc7a5cfca75
Felix Flores <felix@edgecase.com>
2011-06-28 00:07:39 -0400

    Testing login

--- public/javascripts/facebook.js
+++ public/javascripts/facebook.js
@@ -5,38 +5,39 @@ FB.init({
   xfbml:    true
 });
 
-FB.api('/me/friends', function(friends) {
-  if(friends !== null && friends !== undefined) {
-    var friendsIds,
-        today = new Date(),
-        currentDate = today.getDate(),
-        currentMonth = today.getMonth() + 1;
 
-    friendsIds = $.map(friends.data, function(value, index) {
-      return value.id;
-    });
+// FB.getLoginStatus(function(response) {
+//   if (response.session) { $('#login-button').hide(); }
+// });
 
-    $.each(friendsIds, function(index, value) {
-      FB.api('/' + value, function(resp) {
-        if(resp.birthday !== null && resp.birthday !== undefined) {
-          var birthdayArray = resp.birthday.split('/'),
-              userBirthdayMonth = parseInt(birthdayArray[0]),
-              userBirthdayDate = parseInt(birthdayArray[1]);
-
-          if(userBirthdayMonth === currentMonth && userBirthdayDate === currentDate) {
-            console.log("Happy birthday: " + resp.id);
-            FB.api('/' + resp.id + '/feed', 'post', { message: "Happy Birthday :)" }, function(response) {
-              if (!response || response.error) {
-                console.log(response.error);
-              }
-            });
-          }
-        };
-
-      });
-    });
-
-  }
-});
-
+// FB.api('/me/friends', function(friends) {
+//   if(friends !== null && friends !== undefined) {
+//     var today = new Date(),
+//         currentDate = today.getDate(),
+//         currentMonth = today.getMonth() + 1;
+// 
+//     $.each(friends.data, function(index, friend) {
+//       FB.api('/' + friend.id, function(resp) {
+//         if(resp.birthday !== null && resp.birthday !== undefined) {
+//           console.log(resp.birthday);
+//       //     var birthdayArray = resp.birthday.split('/'),
+//       //         userBirthdayMonth = parseInt(birthdayArray[0]),
+//       //         userBirthdayDate = parseInt(birthdayArray[1]);
+// 
+//       //     if(userBirthdayMonth === currentMonth && userBirthdayDate === currentDate) {
+//       //       console.log("Happy birthday: " + resp.id);
+//       //       FB.api('/' + resp.id + '/feed', 'post', { message: "Happy Birthday :)" }, function(response) {
+//       //         if (!response || response.error) {
+//       //           console.log(response.error);
+//       //         }
+//       //       });
+//       //     }
+//         };
+//       });
+// 
+//     });
+// 
+//   }
+// });
+// 
 
